5aec5d089cdeb387f076e6477017b51edaeec543
wuchangsheng <wuchangsheng90@126.com>
2021-04-16 21:59:22 +0800

    Fix Qry Order

--- Interface/include/cwBasicKindleStrategy.h
+++ Interface/include/cwBasicKindleStrategy.h
@@ -67,7 +67,7 @@ public:
 	bool					InitialHisKindleFromTickFile(const char * szTickFile);
 	bool					InitialHisKindleFromIndexFile(const char * szTickFile);
 
-	bool					InitialHisKindleFromDB(const char * szDb);
+	bool					InitialHisKindleFromDB(int iCount);
 
 	//获取已经订阅的k线
 	cwKindleSeriesPtr		GetKindleSeries(const char * szInstrumentID, int iTimeScale);
