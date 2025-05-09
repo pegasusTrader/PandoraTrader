003cb3d9c504eee51aec064b1bef58ecda3a16e8
shibusama <1319119251@SHIBUSAMA>
2025-05-10 02:15:56 +0800

    feat：test

--- PandoraStrategy/cwStrategyDemo.cpp
+++ PandoraStrategy/cwStrategyDemo.cpp
@@ -70,89 +70,89 @@ void cwStrategyDemo::OnBar(cwMarketDataPtr pPriceData, int iTimeScale, cwBasicKi
 	auto minute = _timePara.minute;
 	auto second = _timePara.second;
 
-	if ((hour == 9 && minute >= 1) || (hour > 9 && hour < 10) || (hour == 10 && minute < 15) ||
-		(hour == 10 && minute >= 30) || (hour > 10 && hour < 11) || (hour == 11 && minute < 30) ||
-		(hour == 13 && minute >= 30) || (hour > 13 && hour < 14) || (hour == 14 && minute < 45)) {
-		// 输出当前时间
-		std::cout << "当前时间:需要填写" << std::endl;
-		std::cout << "--- Tigger " << std::setfill('0') << std::setw(2) << hour << ":" << std::setfill('0') << std::setw(2) << minute << ":" << std::setfill('0') << std::setw(2) << second << " ---------------" << std::endl;
-		std::cout << "InstrumentID - Direction - Position - OpenPriceAvg - MktProfit  |  ExchangeMargin - OpenCost --->" << std::endl;
+	//if ((hour == 9 && minute >= 1) || (hour > 9 && hour < 10) || (hour == 10 && minute < 15) ||
+	//	(hour == 10 && minute >= 30) || (hour > 10 && hour < 11) || (hour == 11 && minute < 30) ||
+	//	(hour == 13 && minute >= 30) || (hour > 13 && hour < 14) || (hour == 14 && minute < 45)) {
+	//	// 输出当前时间
+	//	std::cout << "当前时间:需要填写" << std::endl;
+	//	std::cout << "--- Tigger " << std::setfill('0') << std::setw(2) << hour << ":" << std::setfill('0') << std::setw(2) << minute << ":" << std::setfill('0') << std::setw(2) << second << " ---------------" << std::endl;
+	//	std::cout << "InstrumentID - Direction - Position - OpenPriceAvg - MktProfit  |  ExchangeMargin - OpenCost --->" << std::endl;
 
-		//输出当前持仓情况
-		for (const auto& pair : curPos) {
-			double OpenPriceAvg = curPos[pair.first]->AveragePosPrice;
-			double absMktProfit = curPos[pair.first]->PositionProfit;
-			double MktProfit = curPos[pair.first]->PosiDirection == CW_FTDC_D_Buy ? absMktProfit : -1 * absMktProfit;
-			if (curPos[pair.first]->TodayPosition != 0) {
-				std::cout << "=====" << curPos[pair.first]->InstrumentID << "     " << curPos[pair.first]->PosiDirection << "     " << curPos[pair.first]->TodayPosition
-					<< OpenPriceAvg << "     " << MktProfit << "     " << curPos[pair.first]->ExchangeMargin << "     " << curPos[pair.first]->ExchangeMargin << "     "
-					<< curPos[pair.first]->OpenCost << "     " << std::endl;
-			}
-		}
+	//	//输出当前持仓情况
+	//	for (const auto& pair : curPos) {
+	//		double OpenPriceAvg = curPos[pair.first]->AveragePosPrice;
+	//		double absMktProfit = curPos[pair.first]->PositionProfit;
+	//		double MktProfit = curPos[pair.first]->PosiDirection == CW_FTDC_D_Buy ? absMktProfit : -1 * absMktProfit;
+	//		if (curPos[pair.first]->TodayPosition != 0) {
+	//			std::cout << "=====" << curPos[pair.first]->InstrumentID << "     " << curPos[pair.first]->PosiDirection << "     " << curPos[pair.first]->TodayPosition
+	//				<< OpenPriceAvg << "     " << MktProfit << "     " << curPos[pair.first]->ExchangeMargin << "     " << curPos[pair.first]->ExchangeMargin << "     "
+	//				<< curPos[pair.first]->OpenCost << "     " << std::endl;
+	//		}
+	//	}
 
-		std::vector<cwOrderPtr> orders = cwStrategyDemo::HandBar(code2data/*当前持仓数据*/, curPos);
-		for (const auto& order : orders) {
-			// 增加 sendCount
-			++sendCount;
-			std::cout << "    Count=" << sendCount << "：" << order->InstrumentID << " => " << order->Direction << " " << order->CombOffsetFlag << " - Amount " << order->VolumeTotalOriginal << " Price " << order->LimitPrice << std::endl;
-			std::string offsetFlag;
-			/*		if (order->CombOffsetFlag == "Open") {
-						offsetFlag = "0";
-					}
-					else {
-						auto it = futInfTable.find(order->InstrumentID);
-						if (it != futInfTable.end() && it->second->ExchangeID == "SHFE") {
-							offsetFlag = "3";
-						}
-						else {
-							offsetFlag = "1";
-						}
-					}*/
-			EasyInputMultiOrder(order->InstrumentID, order->VolumeTotalOriginal, order->LimitPrice);
-			if (orders.size() > 0) {
-				std::cout << "--- SUBMIT ORDERS ===========" << std::endl;
-			}
-			std::this_thread::sleep_for(std::chrono::milliseconds(5000)); // 等待 5 秒
-			std::cout << "--- Update curPosInfo ===========" << std::endl; // 更新持仓信息
-		}
-	}
-	else if ((hour == 14 && minute >= 45) || (hour == 15 && minute == 0))
-	{
-		std::cout << "dfff" << std::endl;
-		for (const auto& pair : curPos) {
-			const std::string& codeDR = pair.first;
-			const PositionFieldPtr& position = pair.second;
-			if (position->TodayPosition != 0) {
-				try {
-					sendCount++;
-					cwOrderPtr order;
-					*order->InstrumentID = *position->InstrumentID;
-					order->VolumeTotalOriginal = position->TodayPosition;
-					cwFtdcDirectionType Long = '0';
-					order->Direction = (position->PosiDirection == Long) ? CW_FTDC_D_Buy : CW_FTDC_D_Sell;
-					std::string offsetFlag = (futInfTable[order->InstrumentID]->ExchangeID == "SHFE") ? "3" : "1";
-					order->LimitPrice = (order->Direction == CW_FTDC_D_Buy) ? code2data[order->InstrumentID]->AskPrice1 + 2 * futInfTable[order->InstrumentID]->PriceTick : code2data[order->InstrumentID]->BidPrice1 - 2 * futInfTable[order->InstrumentID]->PriceTick;
-					EasyInputOrder(order->InstrumentID, order->Direction, order->LimitPrice);
-				}
-				catch (const std::exception& ex) {
-					std::cout << "ERROR " << codeDR << " ------------" << std::endl;
-					std::cout << ex.what() << std::endl;
-				}
-			}
-		}
-	}
-	else if ((hour == 15 && minute >= 0 && minute < 10))
-	{
-		std::cout << "----------------- TraderOver ----------------" << std::endl;
-		std::cout << "--------------- StoreBaseData ---------------" << std::endl;
-		std::cout << "---------------------------------------------" << std::endl;
-	}
-	else
-	{
-		if (minute == 0 || minute == 10 || minute == 20 || minute == 30 || minute == 40 || minute == 50) {
-			std::cout << "waiting" << hour << "::" << minute << "::" << second << std::endl;
-		}
-	}
+	//	std::vector<cwOrderPtr> orders = cwStrategyDemo::HandBar(code2data/*当前持仓数据*/, curPos);
+	//	for (const auto& order : orders) {
+	//		// 增加 sendCount
+	//		++sendCount;
+	//		std::cout << "    Count=" << sendCount << "：" << order->InstrumentID << " => " << order->Direction << " " << order->CombOffsetFlag << " - Amount " << order->VolumeTotalOriginal << " Price " << order->LimitPrice << std::endl;
+	//		std::string offsetFlag;
+	//		/*		if (order->CombOffsetFlag == "Open") {
+	//					offsetFlag = "0";
+	//				}
+	//				else {
+	//					auto it = futInfTable.find(order->InstrumentID);
+	//					if (it != futInfTable.end() && it->second->ExchangeID == "SHFE") {
+	//						offsetFlag = "3";
+	//					}
+	//					else {
+	//						offsetFlag = "1";
+	//					}
+	//				}*/
+	//		EasyInputMultiOrder(order->InstrumentID, order->VolumeTotalOriginal, order->LimitPrice);
+	//		if (orders.size() > 0) {
+	//			std::cout << "--- SUBMIT ORDERS ===========" << std::endl;
+	//		}
+	//		std::this_thread::sleep_for(std::chrono::milliseconds(5000)); // 等待 5 秒
+	//		std::cout << "--- Update curPosInfo ===========" << std::endl; // 更新持仓信息
+	//	}
+	//}
+	//else if ((hour == 14 && minute >= 45) || (hour == 15 && minute == 0))
+	//{
+	//	std::cout << "dfff" << std::endl;
+	//	for (const auto& pair : curPos) {
+	//		const std::string& codeDR = pair.first;
+	//		const PositionFieldPtr& position = pair.second;
+	//		if (position->TodayPosition != 0) {
+	//			try {
+	//				sendCount++;
+	//				cwOrderPtr order;
+	//				*order->InstrumentID = *position->InstrumentID;
+	//				order->VolumeTotalOriginal = position->TodayPosition;
+	//				cwFtdcDirectionType Long = '0';
+	//				order->Direction = (position->PosiDirection == Long) ? CW_FTDC_D_Buy : CW_FTDC_D_Sell;
+	//				std::string offsetFlag = (futInfTable[order->InstrumentID]->ExchangeID == "SHFE") ? "3" : "1";
+	//				order->LimitPrice = (order->Direction == CW_FTDC_D_Buy) ? code2data[order->InstrumentID]->AskPrice1 + 2 * futInfTable[order->InstrumentID]->PriceTick : code2data[order->InstrumentID]->BidPrice1 - 2 * futInfTable[order->InstrumentID]->PriceTick;
+	//				EasyInputOrder(order->InstrumentID, order->Direction, order->LimitPrice);
+	//			}
+	//			catch (const std::exception& ex) {
+	//				std::cout << "ERROR " << codeDR << " ------------" << std::endl;
+	//				std::cout << ex.what() << std::endl;
+	//			}
+	//		}
+	//	}
+	//}
+	//else if ((hour == 15 && minute >= 0 && minute < 10))
+	//{
+	//	std::cout << "----------------- TraderOver ----------------" << std::endl;
+	//	std::cout << "--------------- StoreBaseData ---------------" << std::endl;
+	//	std::cout << "---------------------------------------------" << std::endl;
+	//}
+	//else
+	//{
+	//	if (minute == 0 || minute == 10 || minute == 20 || minute == 30 || minute == 40 || minute == 50) {
+	//		std::cout << "waiting" << hour << "::" << minute << "::" << second << std::endl;
+	//	}
+	//}
 };
 
 void cwStrategyDemo::OnRtnTrade(cwTradePtr pTrade)
--- PandoraStrategy/cwStrategyDemo.h
+++ PandoraStrategy/cwStrategyDemo.h
@@ -29,24 +29,4 @@ public:
 
 	std::string m_strCurrentUpdateTime;
 
-	static double ArithmeticMean(const std::vector<double>& arr); //计算简单算数平均值
-
-	static double SampleStd(const std::vector<double>& arr); //计算样本标准差
-
-	static std::string GetTodayDate();
-
-	static void UpdateBarData();// 加载历史信息
-
-	static void UpdateFlow(std::unordered_map<std::string, cwMarketDataPtr> code2data, std::unordered_map<std::string, PositionFieldPtr> curPos);// 记录最新持仓状况（方向，数量，成本价格，开仓成本，数量）
-
-	static std::vector<cwOrderPtr> StrategyTick(std::unordered_map<std::string, cwMarketDataPtr> code2data/*数据*/);
-
-	static std::vector<cwOrderPtr> StrategyPosOpen(std::string contract, cwMarketDataPtr barBook, double stdLong, double stdShort);
-
-	static std::vector<cwOrderPtr> StrategyPosClose(std::string contract, cwMarketDataPtr barBook, double stdLong, double stdShort);
-
-	static std::vector<cwOrderPtr> StrategyPosSpeC(std::string contract, cwMarketDataPtr barBook, long posO);
-
-	static std::vector<cwOrderPtr> HandBar(std::unordered_map<std::string, cwMarketDataPtr> code2data/*昨仓数据*/, std::unordered_map<std::string, PositionFieldPtr> curPos);
-
 };
